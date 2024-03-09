151b92fcd59785c2e9cf7119328230251c7c7236
Bca202100713 <202100713@sigc.edu>
2024-03-09 03:45:20 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="import React from 'react'
+
+export default function Update() {
+  return (
+    <div>Update</div>
+  )
+}
+"alt="Welcome picture" />
+    </div>
+  )
+}

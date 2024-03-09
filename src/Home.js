ae7fa83b37fb2b99df6c8eeb32e368cb1475aa7d
Bca202100713 <202100713@sigc.edu>
2024-03-09 04:13:23 -0800

    first commit

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,7 @@ export default function Home() {
   return (
     <div className="home-container">
       <h3>Welcome to React-App</h3>
-      <img className="home-img" src="import React from 'react'
-
-export default function Update() {
-  return (
-    <div>Update</div>
-  )
-}
-"alt="Welcome picture" />
+      <img className="home-img" src="https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340" alt="Welcome picture"/>
     </div>
   )
 }
